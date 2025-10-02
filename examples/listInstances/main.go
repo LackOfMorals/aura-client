85c7e6db0c0bc1841bc0cf788600d6eb0a0f2c3d
Jonathan Giffard <39745127+LackOfMorals@users.noreply.github.com>
2025-10-02 17:09:36 +0100

    Added listInstances with example

---
+++ examples/listInstances/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"os"
+
+	auraAPIClient "github.com/LackOfMorals/aura-api-client/auraAPIClient"
+)
+
+const (
+	AuraAPIBaseURL      = "https://api.neo4j.io/"
+	AuraAPIAuthEndpoint = "oauth/token"
+	AuraAPIV1           = "v1"
+)
+
+func main() {
+
+	// Read ClientID, ClientSecret from env vars of the same name
+	ClientID, ClientSecret, err := readClientIDAndSecretFromEnv()
+	if err != nil {
+		log.Println("Unable to obtain values for authentication to Aura API: ", err)
+		os.Exit(1)
+	}
+
+	myAuraClient := auraAPIClient.NewAuraAPIActionsService(AuraAPIBaseURL, AuraAPIV1, "120", ClientID, ClientSecret)
+
+	auraToken, err := myAuraClient.GetAuthToken()
+	if err != nil {
+		log.Println("Error getting token: ", err)
+		os.Exit(1)
+	}
+
+	response, err := myAuraClient.ListInstances(auraToken)
+	if err != nil {
+		log.Println("Error reading instances: ", err)
+		os.Exit(1)
+	}
+
+	result, err := json.MarshalIndent(response, "", "  ")
+	if err != nil {
+		log.Println("Error formatting response: ", err)
+		os.Exit(1)
+	}
+
+	log.Printf("Instance details: %s", result)
+
+}
+
+func readClientIDAndSecretFromEnv() (string, string, error) {
+	var ClientID, ClientSecret string
+	var found bool
+
+	// Is set by LookupEnv to true if the environmental variable is found
+	found = false
+
+	// See if environmantal variables are present and get their value if so
+	// set found to true if this is the case
+	ClientID, found = os.LookupEnv("ClientID")
+	if !found {
+		return "", "", errors.New("environmental variable ClientID not set")
+	}
+
+	ClientSecret, found = os.LookupEnv("ClientSecret")
+	if !found {
+		return "", "", errors.New("environmental variable ClientSecret not set")
+	}
+
+	return ClientID, ClientSecret, nil
+}
