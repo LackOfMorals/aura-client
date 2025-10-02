1bc8820f3a8e17e93b3db5b974abf7d6e5ea030f
Jonathan Giffard <39745127+LackOfMorals@users.noreply.github.com>
2025-10-02 17:39:55 +0100

    httpClient refactor.  merged interfaces and service into the same file.  they are small and there was no advantage in keeping them seperate.  bringing together improves readability

--- auraAPIClient/internal/httpClient/httpClient.go
+++ auraAPIClient/internal/httpClient/httpClient.go
@@ -9,6 +9,16 @@ import (
 	"net/http"
 )
 
+// Defines the interface for making HTTP Requests
+type HTTPRequestExecutor interface {
+	MakeRequest(endpoint string, method string, header map[string][]string, body []byte) (*HTTPResponse, error)
+}
+
+// HTTP service
+type HTTPService interface {
+	HTTPRequestExecutor
+}
+
 // Stores the response from a request. Includes the original response
 type HTTPResponse struct {
 	ResponsePayload *[]byte
