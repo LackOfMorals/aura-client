7d992e860887f7439fcaf9dca9a051a7d86b98f8
Jonathan Giffard <39745127+LackOfMorals@users.noreply.github.com>
2026-04-29 21:06:32 +0100

    Updated client version

--- client.go
+++ client.go
@@ -36,7 +36,7 @@ const auraAPIVersion = "v1"
 
 // AuraAPIClientVersion is the current release version of this library.
 // Updated via changie on each release.
-const AuraAPIClientVersion = "v1.8.2"
+const AuraAPIClientVersion = "v1.9.0"
 
 // ============================================================================
 // Client types
